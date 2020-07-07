119f12072be95c5b72229c824417234a69c62e57
Takehiko Mori <mori.t.bb@m.titech.ac.jp>
2020-07-07 11:26:07 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,33 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) {
+	int i;
+	int sum = 0;
+	for (i = 1; i <= n; i++) {
+		sum += A[i] / m;
+	}
+	return(int)sum >= k;
+}
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
 
-
-  return 0;
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	lb = 0;
+	ub = 1000000001;
+	while (ub - lb > 1) {
+		int mid = (lb + ub) / 2;
+		if (p(mid)) {
+			lb = mid;
+		}
+		else {
+			ub = mid;
+		}
+	}
+	printf("%d\n", lb);
+	return 0;
 }
