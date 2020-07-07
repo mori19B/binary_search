3e984854ccd71aa72ec71e9309cfe9fd6723f500
Takehiko Mori <mori.t.bb@m.titech.ac.jp>
2020-07-07 11:25:43 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) {
+	int i;
+	int sum = 0;
+	for (i = 1; i <= n; i++) {
+		sum += (A[i] + m - 1) / m;
+	}
+	return(int)sum <= k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +20,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000001;
+  while (ub - lb > 1) {
+	  int mid = (lb + ub) / 2;
+	  if (p(mid)) {
+		  ub = mid;
+	  } 
+	  else {
+		  lb = mid;
+	  }
+  }
+  printf("%d\n", ub);
   return 0;
 }
